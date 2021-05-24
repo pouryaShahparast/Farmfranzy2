53e35a386d305d0d94cbb5b5a8da8aa4b5bb1ed7
[pourya shahparast] <[pouryashahparast@gmail.com]>
2021-05-24 16:12:14 +0430

    Grass//completed

---
+++ src/Grass.java
@@ -0,0 +1,16 @@
+public class Grass {
+    private int xCoordinate;
+    private int yCoordinate;
+
+    public int getXCoordinate() {
+        return xCoordinate;
+    }
+
+    public int getYCoordinate() {
+        return yCoordinate;
+    }
+    public Grass(int xLocation, int yLocation) {
+        this.xCoordinate = xLocation;
+        this.yCoordinate = yLocation;
+    }
+}
